d6aa5c6eb18d314e9e8ec36fcf281bab75052134
akvel <mtakvel@gmail.com>
2013-06-05 21:00:01 +0400

    Import

---
+++ README.md
@@ -0,0 +1,15 @@
+Timecon
+=======
+
+Project get your system log events and parse it for know how many hours you work in this month.
+
+FYI It can not works if  you do not shutdown or turn to sleep mode your PC after work.
+
+
+
+Requirements:
+* JDK6 or hider
+* Windows 7
+* added path to jdk6/bin to env variable PATH
+
+
---
+++ Timecon.bat
@@ -0,0 +1,18 @@
+@echo off
+del ev.txt 2> nul
+
+wevtutil qe Security /rd:false /f:text "/q:*[EventData[Data[@Name='LogonType']=7 or Data[@Name='LogonType']=2] or System[Task=13568]]">events_1.txt
+wevtutil qe System /rd:false /f:text "/q:*[System[Provider[@Name='Microsoft-Windows-Kernel-General']]]">events_2.txt
+
+javac Timecon.java
+java Timecon
+
+del Timecon.class
+del events_1.txt
+del events_2.txt
+
+call notepad ev.txt
+
+del ev.txt
+
+
